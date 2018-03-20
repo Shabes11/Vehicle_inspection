1f07306f07ac008b6ff746318959222b160efc9c
tmp-sda-1161 <tmp-sda-1161@sda-25.csc.kth.se>
2018-03-20 09:40:12 +0100

    created Print and Money

---
+++ VehicleInspection/src/se/kth/sda/queuenum/controller/Controller.java
@@ -0,0 +1,19 @@
+package se.kth.sda.queuenum.controller;
+
+
+
+
+/**
+ * This is the application's only controller. All calls to the model pass through here.
+ */
+public class Controller {
+    
+    
+    /**
+     * Instantiates a new Controller and creates a {@link Display} object.
+     */
+    public Controller() {
+        
+    }
+
+}
---
+++ VehicleInspection/src/se/kth/sda/queuenum/startup/Main.java
@@ -0,0 +1,30 @@
+package se.kth.sda.queuenum.startup;
+
+import se.kth.sda.queuenum.integration.CheckList;
+import se.kth.sda.queuenum.integration.CheckListItem;
+
+
+/**
+ * Starts the application.
+ */
+public class Main {
+    /**
+     * This is the main method of the entire application.
+     * 
+     * @param args There are no command line arguments.
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+    
+    public void createCheckList(){
+//        CheckList checkList = new CheckList();
+//        
+//            
+//            checkList.add(new CheckListItem("wheel", 20));
+            
+        
+    }
+
+}
